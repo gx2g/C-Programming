88ca36aa953f937c6f5f4c44e56b3ce8c20984b5
Robert Resendez <grafx2g@gmail.com>
2026-03-28 12:17:28 -0700

    C2A1E1 Projects

---
+++ assignments/C2_Assignment_1_Examples.cpp
@@ -0,0 +1,12 @@
+//
+// Additional examples are not provided for "C/C++ Programming II" Assignment 1.
+//
+// Assignment 1 is a pre-test based on concepts with which students are expected
+// to already be familiar based upon having met the course's prerequisites.  I
+// recommend reviewing sections 1-10 of the course book and asking questions if
+// you need additional help.
+// 
+// Additional examples are provided for "C/C++ Programming II" Assignments 2-8.
+//
+int main(void) {} // Do nothing code just to avoid a linker error
+//
